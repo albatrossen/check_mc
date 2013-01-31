d663fbe5ee83d1aac057c9311dd25417dafc4161
Jes Andersen <jeand@kk-electronic.com>
2013-01-31 17:24:07 +0100

    Added option to ignore player chat lines

--- check_mc.py
+++ check_mc.py
@@ -7,10 +7,11 @@ parser = argparse.ArgumentParser(
     fromfile_prefix_chars='@'
     )
 parser.add_argument('logfile',type=argparse.FileType('r'),help='the logfile to process')
+parser.add_argument('--ignore', action='store', nargs='*',default=[],help='lines to ignore')
 parser.add_argument('--good', action='store', nargs='*',default=[],help='Immediately return 0 if one of these is found (server restart)')
 parser.add_argument('--bad', action='store', nargs='*',default=[],help='Reuire none of these regex to return 0 (exception watcher)')
 parser.add_argument('--hearthbeat', action='store', nargs='*',default=[],help='Require one of these regex to return 0 (to ensure no deadlocks)')
-parser.add_argument('--searchwindow', action='store',type=int, default=5*60,help='Seconds it should search back in the log')
+parser.add_argument('--searchwindow', action='store',type=int, default=5*60,help='Seconds it should search back in the log (default 5min)')
 parser.add_argument('--timesplit', action='store', default='(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d) (.*)',help=argparse.SUPPRESS)
 parser.add_argument('--timeformat', action='store', default='%Y-%m-%d %H:%M:%S',help=argparse.SUPPRESS)
 
@@ -32,6 +33,14 @@ stoptime = time.time() - options.searchwindow
 regex_hearthbeat = map(re.compile,options.hearthbeat)
 regex_good = map(re.compile,options.good)
 regex_bad = map(re.compile,options.bad)
+regex_ignore = map(re.compile,options.ignore)
+
+def any_match(regexes,line):
+	for regex in regexes:
+		if regex.search(line):
+			return True
+	return False
+
 
 def find_health(logfile):
 	retval = 2 if regex_hearthbeat else 0
@@ -42,15 +51,14 @@ def find_health(logfile):
 			timestamp = time.mktime(time.strptime(timestamp_string,options.timeformat))
 			if timestamp <= stoptime:
 				return retval
-		for regex in regex_good:
-			if regex.search(line):
-				return 0
-		for regex in regex_hearthbeat:
-			if regex.search(line):
-				retval = 0
-		for regex in regex_bad:
-			if regex.search(line):
-				return 1
+		if any_match(regex_ignore,line):
+			continue
+		if any_match(regex_good,line):
+			return 0
+		if any_match(regex_hearthbeat,line):
+			retval = 0
+		if any_match(regex_bad,line):
+			return 1
 	return retval
 
 exit(find_health(options.logfile))
